adb55ce8ee5027fccfb9120cf2ef2314dfa38f38
backtestdev <rblake2838@gmail.com>
2026-02-10 11:29:11 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,10 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Always run for Clerk proxy + API routes
+    "/(__clerk|api|trpc)(.*)",
+    // Everything else except Next internals + static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
   ],
 };
+
